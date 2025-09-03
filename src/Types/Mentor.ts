9fe70a053d4cfbb80bb2dd0c215973b4f6010300
gentelmayor-sudo <gentelmayor911@gmail.com>
2025-09-03 11:06:07 +0100

    worked on mentor card

--- src/Types/Mentor.ts
+++ src/Types/Mentor.ts
@@ -1,12 +1,17 @@
-// src/Types/Mentor.ts
-
 export interface Mentor {
-  id: string;
-  name: string;
-  role: string;
-  rating: number;
-  reviews: { id: string; text: string; rating: number }[];
-  bio: string;
-  portfolio: string;
-  image: string;
+  [x: string]: ReactNode;
+  id: string; 
+  firstName: string; 
+  lastName: string; 
+  email: string; 
+  userName: string; 
+  createdAt: string; 
+  updatedAt: string; 
+  __v: number; 
+  name?: string; 
+  rating?: number; 
+  reviews?: { id: string; text: string; rating: number }[]; 
+  bio?: string; 
+  portfolio?: string; 
+  image?: string; 
 }
