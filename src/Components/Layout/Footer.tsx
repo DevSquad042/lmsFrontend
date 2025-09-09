4fde8358f45c0b9315a4032d0acb3caf91c38b00
frankly-Droid <franklydemand@gmail.com>
2025-09-09 11:08:52 +0100

    update

--- src/Components/Layout/Footer.tsx
+++ src/Components/Layout/Footer.tsx
@@ -9,11 +9,11 @@ import { Link } from 'react-router-dom';
 
 const Footer: React.FC = () => {
   return (
-    <div className='Footer-container'>
+    <footer className='Footer-container'>
       <div className='Sub-container'>
-        <img src={Logo} alt="Logo" />
-        <p>Empowering learners through accessible and engaging online  <br />education.</p>
-        <p>Byway is a leading online learning platform dedicated to <br /> providing high-quality, flexible, and affordable educational <br /> experiences.</p>
+        <img src={Logo} alt="Byway Logo" />
+        <p>Empowering learners through accessible and engaging online education.</p>
+        <p>Byway is a leading online learning platform dedicated to providing high-quality, flexible, and affordable educational experiences.</p>
       </div>
 
       <div className='Sub-container'>
@@ -29,7 +29,7 @@ const Footer: React.FC = () => {
         </Link>
       </div>
 
-      <div  className='Sub-container'>
+      <div className='Sub-container'>
         <h3>Programs</h3>
         <p>Art & Design</p>
         <p>Business</p>
@@ -38,31 +38,30 @@ const Footer: React.FC = () => {
         <p>Programming</p>
       </div>
 
-      <div  className='Sub-container2'>
+      <div className='Sub-container2'>
         <h3>Contact Us</h3>
-        <p>Kilometer 7 Enugu portharcourt express way <br /> centinary city Enugu</p>
+        <p>Kilometer 7 Enugu Port Harcourt Express Way, Centenary City Enugu</p>
         <p>Tel: +(123) 907-798-3405</p>
         <p>Mail: bywatedu@webkul.in</p>
         <div className='Icons'>
-         <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-  <FaFacebook />
-</a>
-<a href="https://www.github.com" target="_blank" rel="noopener noreferrer">
-  <FaGithub />
-</a>
-<a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
-  <FcGoogle />
-</a>
-<a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-  <FaXTwitter />
-</a>
-<a href="https://www.microsoft.com" target="_blank" rel="noopener noreferrer">
-  <FaMicrosoft />
-</a>
-
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+            <FaFacebook />
+          </a>
+          <a href="https://www.github.com" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+            <FaGithub />
+          </a>
+          <a href="https://www.google.com" target="_blank" rel="noopener noreferrer" aria-label="Google">
+            <FcGoogle />
+          </a>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+            <FaXTwitter />
+          </a>
+          <a href="https://www.microsoft.com" target="_blank" rel="noopener noreferrer" aria-label="Microsoft">
+            <FaMicrosoft />
+          </a>
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
